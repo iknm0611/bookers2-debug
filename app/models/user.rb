b0b7a9a895776b005b20e18b2358c7c7dea3a4d0
iknm0611 <iknm0611@gmail.com>
2021-09-08 06:36:37 +0000

    応用課題４提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,25 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :relationships, foreign_key: :follower_id
+  has_many :followings, through: :relationships, source: :followed
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  def is_followed_by?(user)
+    self.followings.include?(user)
+  end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
-  validates :introduction, length: {maximum: 50}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
 end
